e81896cad6c89f0f43c0343db664bce0585ffe82
RawatRicha <rawricha841@gmail.com>
2021-07-23 16:54:06 +1000

    first commit

---
+++ calculate_water_overflow.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+CLI interface to calculate the overflow.
+"""
+
+import argparse
+import src.overflow as overflow
+from src.glass import Glass
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--row", default=0, type=int,
+                        help="The row of the glass.")
+    parser.add_argument("-j", "--col", default=0, type=int,
+                        help="The col of the glass.")
+    parser.add_argument("-k", "--water", default=0, type=float,
+                        help="The amount of water we would like to pour into the top glass")
+    parser.add_argument('-v', '--visualize', action="store_true",
+                        help="Flag for if we want to visualize the tree.")
+    return parser.parse_args()
+
+
+args = get_args()
+i = args.row
+j = args.col
+k = args.water
+should_visualize = args.visualize
+
+if __name__ == "__main__":
+
+    if should_visualize:
+        g = Glass()
+        g.fill(k)
+        overflow.illustrate(g)
+
+    y = overflow.calculate(i, j, k)
